3e46ad13a5a75942035aa7ae91223a7a361a12b2
ataoldotcom <78866932+ataoldotcom@users.noreply.github.com>
2026-04-27 15:17:13 -0500

    incorporating kafka w/ flush
    right now every time a dog is detected,
    1. create a message
    2. send it to kafka
    3. wait (flush) until kafka confirms it received it.
    
    need to add async

--- app/kafka_producer.py
+++ app/kafka_producer.py
@@ -1,1 +1,25 @@
 
+import json
+from datetime import datetime
+from confluent_kafka import Producer
+
+producer = Producer({
+    "bootstrap.servers": "localhost:9092"
+})
+
+TOPIC = "detections"
+
+
+def send_event(confidence):
+    event = {
+        "event_type": "dog_detected",
+        "confidence": confidence,
+        "timestamp": datetime.utcnow().isoformat(),
+        "source": "webcam_1"
+    }
+
+    producer.produce(
+        TOPIC,
+        value=json.dumps(event).encode("utf-8")
+    )
+    producer.flush()
